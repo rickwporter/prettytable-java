f953d88be8176e9d4ef9058fe903aec816ed769d
Rick Porter <rickwporter@gmail.com>
2023-03-13 19:31:31 -0400

    PrettyTable implementation

---
+++ src/main/java/com/rickwporter/prettytable/PrettyTable.java
@@ -0,0 +1,282 @@
+package com.rickwporter.prettytable;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class PrettyTable {
+    private static final String HTML_TABLE_TAG = "table";
+    private static final String HTML_HEADER_TAG = "thead";
+    private static final String HTML_BODY_TAG = "tbody";
+    private static final String HTML_ROW_TAG = "tr";
+    private static final String HTML_CELL_BODY_TAG = "td";
+    private static final String HTML_CELL_HEADER_TAG = "th";
+ 
+    public enum CellFormat {
+        CENTER,
+        LEFT,
+        RIGHT, 
+    }
+
+    public enum OutputFormat { 
+        TEXT,
+        CSV,
+        HTML,
+        JSON,
+    }
+
+    private List<String> headers = new ArrayList<>();
+    private List<List<Object>> rows = new ArrayList<>();
+    private List<CellFormat> formats = new ArrayList<>();
+    private CellFormat defaultFormat = CellFormat.CENTER;
+
+    public PrettyTable(String... headers) {
+        this.headers.addAll(Arrays.asList(headers));
+    }
+
+     public void addRow(Object... row) {
+        this.rows.add(Arrays.asList(row));
+    }
+
+    public int getRowCount() {
+        return this.rows.size();
+    }
+
+    public List<Object> getRow(int rowIndex) {
+        if (this.rows.size() <= rowIndex) {
+            return null;
+        }
+        return this.rows.get(rowIndex);
+    }
+
+    public String getCell(int rowIndex, int columnIndex) {
+        List<Object> row = this.getRow(rowIndex);
+        if (row == null || row.size() <= columnIndex) {
+            return null;
+        }
+        return row.get(columnIndex).toString();
+    }
+
+    public void setFormat(int column, CellFormat fmt) {
+        for (int cIdx = this.formats.size(); cIdx <= column; cIdx++) {
+            this.formats.add(this.defaultFormat);
+        }
+        this.formats.set(column, fmt);
+    }
+
+    public void setFormats(CellFormat... formats) {
+        this.formats.clear();
+        this.formats.addAll(Arrays.asList(formats));
+    }
+
+    private CellFormat getFormat(int column) {
+        if (this.formats.size() <= column) {
+            return this.defaultFormat;
+        }
+        return this.formats.get(column);
+    }
+
+    public void removeRedundant() {
+        // Remove duplicate data from rows for more aesthetically pleasing output for text 
+        if (this.rows.size() < 2) {
+            return;
+        }
+        List<Object> lastRow = new ArrayList<Object>(this.rows.get(0));
+        int lastChanged = 0;
+        for (int rIdx = 1; rIdx < this.rows.size(); rIdx++) {
+            List<Object> row = this.rows.get(rIdx);
+            List<Object> rowCopy = new ArrayList<Object>(row);
+            int changed = 0;
+            for (int cIdx = 0; cIdx < row.size(); cIdx++) {
+                if (!row.get(cIdx).equals(lastRow.get(cIdx))) {
+                    break;
+                }
+                row.set(cIdx, "");
+                // blank out current cell
+                changed += 1;
+            }
+            if (changed == 0 || changed <= lastChanged) {
+                lastRow = rowCopy;
+            }
+            lastChanged = changed;
+        }
+    }
+
+    private int getMaxSizeForColumn(int column) {
+        int maxSize = this.headers.get(column).length();
+        for (List<Object> row : this.rows) {
+            int rowSize = row.get(column).toString().length();
+            if (rowSize > maxSize) {
+                maxSize = rowSize;
+            }
+        }
+        return maxSize;
+    }
+
+    private List<Integer> getMaxSizes() {
+        List<Integer> maxSizes = new ArrayList<>();
+        for (int cIdx = 0; cIdx < this.headers.size(); cIdx++) {
+            maxSizes.add(getMaxSizeForColumn(cIdx));
+        }
+        return maxSizes;
+    }
+
+    private String textRow(List<? extends Object> row, List<Integer> maxSizes) {
+        StringBuilder result = new StringBuilder();
+        result.append("|");
+        for (int cIdx = 0; cIdx < row.size(); cIdx++) {
+            String cValue = row.get(cIdx).toString();
+            Integer cSize = maxSizes.get(cIdx);
+            switch (getFormat(cIdx)) {
+            case LEFT:
+                result.append(" " + StringUtils.rightPad(cValue, cSize) + " ");
+                break;
+            case RIGHT:
+                result.append(" " + StringUtils.leftPad(cValue, cSize) + " ");
+                break;
+            case CENTER:
+                result.append(StringUtils.center(cValue, cSize + 2));
+                break;
+            }
+            result.append("|");
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    private String textRule(List<Integer> maxSizes) {
+        StringBuilder result = new StringBuilder();
+        result.append("+");
+        for (int i = 0; i < this.headers.size(); i++) {
+            for (int j = 0; j < maxSizes.get(i) + 2; j++) {
+                result.append("-");
+            }
+            result.append("+");
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    String toText() {
+        List<Integer> maxSizes = getMaxSizes();
+        StringBuilder result = new StringBuilder();
+        result.append(textRule(maxSizes));
+        result.append(textRow(this.headers, maxSizes));
+        result.append(textRule(maxSizes));
+        for (List<Object> row : this.rows) {
+            result.append(textRow(row, maxSizes));
+        }
+        result.append(textRule(maxSizes));
+        return result.toString();
+    }
+
+    String csvEncode(String orig) {
+        if (!orig.contains(",")) {
+            return orig;
+        }
+        // put the original string in quotes
+        return "\"" + orig + "\"";
+    }
+
+    String csvRow(List<? extends Object> row) {
+        return StringUtils.join( 
+            row.stream().map(c -> csvEncode(c.toString())).collect(Collectors.toList()), ","
+        ) + "\n"; 
+    }
+
+    String toCsv() {
+        StringBuilder result = new StringBuilder();
+        result.append(csvRow(this.headers));
+        for (List<Object> row: this.rows) {
+            result.append(csvRow(row));
+        }
+        return result.toString();
+    }
+
+    String htmlRow(List<? extends Object> row, String initIndent, String indent, String cellTag) {
+        // TODO: HTML formatting
+        String result = String.format("%s<%s>\n", initIndent, HTML_ROW_TAG);
+        for (Object col : row) {
+            result += String.format( "%s%s<%s>%s</%s>\n", initIndent, indent, cellTag, col.toString(), cellTag );
+        }
+        result += String.format("%s</%s>\n", initIndent, HTML_ROW_TAG);
+        return result;
+    }
+
+    String toHtml() {
+        StringBuilder result = new StringBuilder();
+        String indent = "    ";
+        result.append(String.format("<%s>\n", HTML_TABLE_TAG));
+        result.append(String.format("%s<%s>\n", indent, HTML_HEADER_TAG));
+        result.append(htmlRow(this.headers, indent + indent, indent, HTML_CELL_HEADER_TAG));
+        result.append(String.format("%s</%s>\n", indent, HTML_HEADER_TAG));
+        result.append(String.format("%s<%s>\n", indent, HTML_BODY_TAG));
+        for (List<Object> row : this.rows) {
+            result.append(htmlRow(row, indent + indent, indent, HTML_CELL_BODY_TAG));
+        }
+        result.append(String.format("%s</%s>\n", indent, HTML_BODY_TAG));
+        result.append(String.format("</%s>\n", HTML_TABLE_TAG));
+        return result.toString();
+    }
+
+    String jsonEncode(Object object) {
+        if (object instanceof Integer) {
+            return object.toString();
+        }
+        try {
+            Integer value = Integer.parseInt(object.toString());
+            return value.toString(); 
+        } catch (NumberFormatException ex) {
+            // nothing to do here, just double-quote as below
+        }
+        return String.format("\"%s\"", object.toString());
+    }
+
+    String jsonRow(List<Object> row, String initIndent, String indent) {
+        List<String> rowValues = new ArrayList<>();
+        for (int cIdx = 0; cIdx < row.size(); cIdx++) {
+            rowValues.add( 
+                String.format("%s%s\"%s\": %s", initIndent, indent, this.headers.get(cIdx), jsonEncode(row.get(cIdx)))
+            );
+        }
+        return StringUtils.join(rowValues, ",\n");
+    }
+
+    String toJson() {
+        StringBuilder result = new StringBuilder();
+        String indent = "    ";
+        result.append("[\n");
+        result.append(String.format("%s[\n", indent));
+        List<String> headerValues = this.headers.stream()
+            .map(h -> String.format("%s%s\"%s\"", indent, indent, h))
+            .collect(Collectors.toList());
+        result.append(StringUtils.join(headerValues, ",\n") + "\n");
+        result.append(String.format("%s]", indent));
+        String entryPrefix = ",\n";
+        for (List<Object> row : this.rows) {
+            result.append(String.format("%s%s{\n", entryPrefix, indent));
+            result.append(jsonRow(row, indent, indent) + "\n");
+            result.append(String.format("%s}", indent));
+        }
+        result.append("\n]\n");
+        return result.toString();
+    }
+
+    public String formattedString(OutputFormat format) {
+        switch (format) {
+        case TEXT:
+            this.removeRedundant();
+            return this.toText();
+        case CSV:
+            return this.toCsv();
+        case JSON:
+            return this.toJson();
+        case HTML:
+            return this.toHtml();
+        }
+        return String.format("Unhandled format=%s", format); 
+    }
+}
---
+++ src/test/java/com/rickwporter/prettytable/PrettyTableTest.java
@@ -0,0 +1,203 @@
+package com.rickwporter.prettytable;
+
+import com.rickwporter.prettytable.PrettyTable.CellFormat;
+import com.rickwporter.prettytable.PrettyTable.OutputFormat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PrettyTableTest {
+    Map<OutputFormat, String> OUTPUT_FORMAT_EXTENSIONS = new HashMap<OutputFormat, String>() {{
+        put(OutputFormat.TEXT, "text");
+        put(OutputFormat.CSV, "csv");
+        put(OutputFormat.HTML, "html");
+        put(OutputFormat.JSON, "json");
+    }};
+
+    final String loadFileContent(String filename) {
+        try {
+            URL resource = getClass().getClassLoader().getResource(filename);
+            return Files.readString(Paths.get(resource.toURI()));
+        } catch (Exception ex) {
+            System.out.println(String.format("got exception reading %s:", filename));
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    PrettyTable createBasicTable() {
+        PrettyTable table = new PrettyTable("Column A", "Column 2", "Column III");
+        table.addRow("A", "B", "c");
+        table.addRow(1, 1024, 65535);
+        table.addRow("Washington", "Adams", "Jefferson");
+        return table;
+    }
+
+    PrettyTable createFormattedTable() {
+        PrettyTable table = createBasicTable();
+        table.setFormat(0, CellFormat.LEFT);
+        table.setFormat(2, CellFormat.RIGHT);
+        return table;
+    }
+
+    @Test
+    public void testBasicCsv() {
+        PrettyTable table = createBasicTable();
+        String result = table.toCsv();
+        String expected = loadFileContent("PrettyTable_basic.csv");
+        Assertions.assertEquals(expected, result);
+        // same answer when going through 'formattedString()'
+        result = table.formattedString(PrettyTable.OutputFormat.CSV);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBasicHtml() {
+        PrettyTable table = createBasicTable();
+        String result = table.toHtml();
+        String expected = loadFileContent("PrettyTable_basic.html");
+        Assertions.assertEquals(expected, result);
+        // same answer when going through 'formattedString()'
+        result = table.formattedString(PrettyTable.OutputFormat.HTML);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBasicJson() {
+        PrettyTable table = createBasicTable();
+        String result = table.toJson();
+        String expected = loadFileContent("PrettyTable_basic.json");
+        Assertions.assertEquals(expected, result);
+        // same answer when going through 'formattedString()'
+        result = table.formattedString(PrettyTable.OutputFormat.JSON);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBasicText() {
+        PrettyTable table = createBasicTable();
+        String result = table.toText();
+        String expected = loadFileContent("PrettyTable_basic.text");
+        Assertions.assertEquals(expected, result);
+        // same answer when going through 'formattedString()'
+        result = table.formattedString(PrettyTable.OutputFormat.TEXT);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFormattedText() {
+        PrettyTable table = createFormattedTable();
+        String result = table.toText();
+        String expected = loadFileContent("PrettyTable_formatted.text");
+        Assertions.assertEquals(expected, result);
+        // same answer when going through 'formattedString()'
+        result = table.formattedString(PrettyTable.OutputFormat.TEXT);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testUnformatted() {
+        // this checks all the "unformatted" outputs... using the same data as the basic test for easier comparison
+        Map<OutputFormat, String> formats = new HashMap<OutputFormat, String>() {{
+            put(OutputFormat.CSV, "csv");
+            put(OutputFormat.HTML, "html");
+            put(OutputFormat.JSON, "json");
+        }};
+        for (Map.Entry<OutputFormat, String> entry: formats.entrySet()) {
+            PrettyTable table = createFormattedTable();
+            String expected = loadFileContent("PrettyTable_basic." + entry.getValue());
+            String result = table.formattedString(entry.getKey());
+            Assertions.assertEquals(expected, result);
+        }
+    }
+
+
+     @Test
+     public void testEmpty() {
+        for (Map.Entry<OutputFormat, String> entry: OUTPUT_FORMAT_EXTENSIONS.entrySet()) {
+            PrettyTable table = new PrettyTable("Col1", "Col2", "Col3", "Col4");
+            String expected = loadFileContent("PrettyTable_empty." + entry.getValue());
+            String result = table.formattedString(entry.getKey());
+         Assertions.assertEquals(expected, result);
+        }
+     }
+
+     @Test
+     public void testDuplicated() {
+         for (Map.Entry<OutputFormat, String> entry: OUTPUT_FORMAT_EXTENSIONS.entrySet()) {
+             // create the table each time, since the print may be destructive
+             PrettyTable table = new PrettyTable("Col1", "Col2", "Col3", "Col4");
+             table.addRow("A", "B", "C", "D");
+             table.addRow("A", "B", "C", "Z");
+             table.addRow("A", "B", "F", "E");
+             table.addRow("A", "B", "F", "E"); // causes an empty row -- would be unusal
+
+            String expected = loadFileContent("PrettyTable_duplicate." + entry.getValue());
+            String result = table.formattedString(entry.getKey());
+            Assertions.assertEquals(expected, result);
+         }
+     }
+
+     @Test
+     public void testQuotedCsv() {
+         PrettyTable table = new PrettyTable("one", "two, with comma", "three");
+         table.addRow("a,b", "c", "d,e");
+         String expected = loadFileContent("PrettyTable_quotes.csv");
+         String result = table.formattedString(OutputFormat.CSV);
+        Assertions.assertEquals(expected, result);
+     }
+
+     @Test
+     public void testGetters() {
+         PrettyTable table = createFormattedTable();
+         Assertions.assertEquals(3, table.getRowCount());
+         int row = 0;
+
+         Assertions.assertNotNull(table.getRow(row));
+         Assertions.assertEquals(table.getCell(row, 0), "A");
+         Assertions.assertEquals(table.getCell(row, 1), "B");
+         Assertions.assertEquals(table.getCell(row, 2), "c");
+         Assertions.assertNull(table.getCell(row, 3));
+
+         row = 1;
+         Assertions.assertNotNull(table.getRow(row));
+         Assertions.assertEquals(table.getCell(row, 0), "1");
+         Assertions.assertEquals(table.getCell(row, 1), "1024");
+         Assertions.assertEquals(table.getCell(row, 2), "65535");
+        Assertions.assertNull(table.getCell(row, 3));
+ 
+        row = 2;
+        Assertions.assertNotNull(table.getRow(row));
+        Assertions.assertEquals(table.getCell(row, 0), "Washington");
+        Assertions.assertEquals(table.getCell(row, 1), "Adams");
+        Assertions.assertEquals(table.getCell(row, 2), "Jefferson");
+        Assertions.assertNull(table.getCell(row, 3));
+        
+        row = 3;
+        Assertions.assertNull(table.getRow(row));
+        Assertions.assertNull(table.getCell(row, 0));
+        Assertions.assertNull(table.getCell(row, 1));
+        Assertions.assertNull(table.getCell(row, 2));
+        Assertions.assertNull(table.getCell(row, 3));
+     }
+
+     @Test
+     public void testNumberAsStringJson() {     
+        // NOTES:
+        // 1. make sure a numeric header is still string encoded
+        // 2. make sure hex value is still string encoded
+        PrettyTable table = new PrettyTable("Col 1", "Col 2", "3");
+        table.addRow("a", "9", 11);
+
+        String expected = loadFileContent("PrettyTable_stringnum.json");
+        String result = table.formattedString(OutputFormat.JSON);
+        Assertions.assertEquals(expected, result);
+     }
+}
